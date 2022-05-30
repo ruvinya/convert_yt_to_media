75b7de7202d69c3dd25be3f4f3fd9ab0b50848b8
Arif Enes Aydın <80915215+ruvinya@users.noreply.github.com>
2022-05-30 18:25:31 +0300

    remove unnecessary files

--- yt_to_media.py
+++ yt_to_media.py
@@ -1,6 +1,5 @@
 import tkinter as tk
-import pytube
-import datetime 
+import pytube 
 import os
 
 def convert_seconds(seconds):
@@ -29,34 +28,24 @@ window.grid_columnconfigure(2, weight=1)
 window.grid_columnconfigure(3, weight=1)
 
 # Title
-title = tk.Label(window, text="YOUTUBE MEDIA DOWNLOADER", font=("Mohave Bold", 32))
+title = tk.Label(window, text="Youtube Media Downloader", font=("Oswald Bold", 31))
 title.configure(background='#4a9976')
 title.grid(column=0, row=0, sticky=tk.EW, padx=5, columnspan=4)
 
 # Link Entry
-link_label = tk.Label(window, text="Youtube Link", font=("Mohave Medium Italic", 18))
+link_label = tk.Label(window, text="Youtube Link", font=("Oswald Medium", 20))
 link_label.configure(background='#4a9976')
 link_label.grid(column=0, row=1, sticky=tk.EW, padx=5, columnspan=4)
 
 yt_link = tk.StringVar()
-link = tk.Entry(window, textvariable=yt_link, font=("Courier New", 16))
+link = tk.Entry(window, textvariable=yt_link, font=("Consolas", 16))
 link.configure(background='#57806e')
 link.grid(column=0, row=2, sticky=tk.EW, padx=5, columnspan=4)
 
 # Preferences
-preferences = tk.Label(window, text="Preferences", font=("Mohave SemiBold", 24))
+preferences = tk.Label(window, text="Preferences", font=("Oswald Medium", 20))
 preferences.configure(background='#4a9976')
 preferences.grid(column=0, row=3, sticky=tk.EW, padx=5, columnspan=4)
-
-# Path
-path_label = tk.Label(window, text="Windows Users Folder Name (Needed for Download)", font=("Mohave Medium Italic", 15))
-path_label.configure(background='#4a9976')
-path_label.grid(column=0, row=4, sticky=tk.EW, padx=5, columnspan=4)
-
-path_str = tk.StringVar()
-path = tk.Entry(window, textvariable=path_str, font=("Courier New", 14))
-path.configure(background='#57806e')
-path.grid(column=0, row=5, sticky=tk.EW, padx=5, columnspan=4)
 
 # Res Enable/Disable
 def video_check(selection):
@@ -66,7 +55,7 @@ def video_check(selection):
         res_opt.configure(state='disabled')
 
 # Dropdown Option Select
-option_label = tk.Label(window, text="Option", font=("Mohave Medium Italic", 15))
+option_label = tk.Label(window, text="Option", font=("Oswald Light", 15))
 option_label.configure(background='#4a9976')
 option_label.grid(column=0, row=6, sticky=tk.EW, padx=5, pady=2)
 
@@ -77,7 +66,7 @@ option.configure(background='#57806e', highlightbackground='#4a9976', width=7)
 option.grid(column=0, row=7, sticky=tk.EW, padx=5)
 
 # Dropdown Resolution Select
-res_label = tk.Label(window, text="Resolution", font=("Mohave Medium Italic", 15))
+res_label = tk.Label(window, text="Resolution", font=("Oswald Light", 15))
 res_label.configure(background='#4a9976')
 res_label.grid(column=3, row=6, sticky=tk.EW, padx=5, pady=2)
 
@@ -119,23 +108,20 @@ process_button.grid(column=0, row=8, padx=5, pady=35, sticky=tk.EW)
 process_button.configure(background='#57806e', width=11, height=5)
 
 # Description
-text_label = res_label = tk.Label(window, text="Status", font=("Mohave Medium", 15))
+text_label = res_label = tk.Label(window, text="Status", font=("Oswald Regular", 15))
 text_label.configure(background='#4a9976')
-text_label.grid(column=1, row=8, padx=5, sticky=tk.N, columnspan=2)
+text_label.grid(column=1, row=7, padx=5, sticky=tk.N, columnspan=2)
 
-text = tk.Text(window, wrap='word', height=7, width=22)
+text = tk.Text(window, wrap='word', height=9, width=22)
 text.configure(background='#57806e', state="disabled")
-text.grid(column=1, row=8, ipadx=50, pady=5, sticky=tk.S, columnspan=2)
+text.grid(column=1, row=6, ipadx=50, pady=5, sticky=tk.S, columnspan=2, rowspan=3)
 
 # Download Button
 def download_file():
-    down_dir = "C:\\Users\\" + path.get() + "\\" + "Downloads"
-    cwd = os.getcwd()
-    os.chdir(down_dir)
     # Remove if a file with same name exists
     if os.path.isfile(title + ".mp4"):
         os.remove(title + ".mp4")
-    output.download(output_path=down_dir)
+    output.download(output_path=os.getcwd())
     os.rename(title + ".mp4", title + ".mp3")
     result_var.set("DOWNLOAD COMPLETED!")
 
@@ -146,6 +132,6 @@ download_button.configure(background='#57806e', width=11, height=5)
 # Result 
 result_label = res_label = tk.Label(window, textvariable=result_var, font=("Mohave Medium", 15))
 result_label.configure(background='#4a9976', foreground="#23FF00")
-result_label.grid(column=1, row=9, padx=5, sticky=tk.N, columnspan=2)
+result_label.grid(column=1, row=10, padx=5, sticky=tk.N, columnspan=2)
 
 window.mainloop()
